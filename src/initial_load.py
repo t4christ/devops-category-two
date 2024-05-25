bba839a2f1cca19688cda70bee5524a84ac21b31
t4christ <t4christ@localhost.localdomain>
2024-05-25 17:41:16 +0100

    initial etl pipeline

--- src/initial_load.py
+++ src/initial_load.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = "5433"
+DB_PORT = "5432"
 DB_NAME = os.environ.get('DB_NAME')
 
 table_name = os.environ.get('TABLE_NAME')
