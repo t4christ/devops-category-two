31d637ddb82f302101fb2d8f8bf09ac49d7062b9
t4christ <47334485+t4christ@users.noreply.github.com>
2024-06-11 15:38:28 +0100

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,16 +1,16 @@
-output "instance_ip" {
-  value = aws_instance.web.public_ip
-}
+#output "instance_ip" {
+#  value = aws_instance.web.public_ip
+#}
 
-output "instance_private_ip" {
-  value = aws_instance.web.private_ip
-}
+#output "instance_private_ip" {
+#  value = aws_instance.web.private_ip
+#}
 
-output "instance_ebs" {
-  value = aws_instance.web.ebs_optimized
-}
+#output "instance_ebs" {
+#  value = aws_instance.web.ebs_optimized
+#}
 
 
-output "ebs" {
-  value = aws_ebs_volume.ctwo.type
-}
+#output "ebs" {
+#  value = aws_ebs_volume.ctwo.type
+#}
