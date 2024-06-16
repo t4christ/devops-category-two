8a677b420552c3a115f1b3099914f15c01ca46dd
t4christ <47334485+t4christ@users.noreply.github.com>
2024-06-16 15:28:29 +0100

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "dynamodb-cone-state-lock" {
-  name           = "ctwo"
+  name           = "devopstwo"
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20
